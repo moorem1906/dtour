2c2471c91ed0a3d475d8484e105cfdf95a488420
moorem1906 <moorem1906@gmail.com>
2019-09-29 22:52:21 -0400

    added cookie-session

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -7,6 +9,18 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
+
+//make use of the cookies library
+app.use(
+    cookieSession({
+        //how long the cookie can exist before expiring
+        //last for 30 days before expiring
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+
+        //this key will encript the cookie 
+        keys: []
+    })
+);
 
 //returns a function and then immediately call the app 
 require('./routes/authRoutes')(app);
--- services/passport.js
+++ services/passport.js
@@ -4,20 +4,39 @@ const mongoose = require('mongoose');
 const keys = require('../config/keys');
 const User = require('../models/User');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+    .then(user => {
+        done(null, user);
+    });
+});
 passport.use(new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        let newUser = new User({googleId: profile.id});
+        User.findOne({googleId: profile.id }).then((existingUser) => {
+            if (existingUser) {
+                //we already havve the record
+                //null = everthing went fine with no errors 
+                done(null, existingUser);  
+            } else {
+             //we don't have a user record with this id, make a new record 
+             let newUser = new User({googleId: profile.id}).save()
+                .then(user => done(null, user));
         
-        newUser.save((err, user) => {
-            if (err) throw err;
+             newUser.save((err, user) => {
+                 if (err) throw err;
+     
+                 console.log('Successfully added ', user);
+             });
+            }
 
-            console.log('Successfully added ', user);
-        });
-        // console.log('accessToken', accessToken);
-        // console.log('refreshToken', refreshToken);
-        // console.log('profile', profile);
+        })
+        
     })
 );
