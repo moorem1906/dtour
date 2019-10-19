3ec1e8ea88236c5efb7fade2be5e52b458a0e678
moorem1906 <moorem1906@gmail.com>
2019-10-18 21:59:30 -0400

    Added missing plugins

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,24 +1,24 @@
 import { FETCH_USER } from './types';
 import axios from 'axios';
-import {constants} from '../constants'
+import { constants } from '../constants'
 
 
 
 export const searchDestinations = params => dispatch => {
-    const data = {
-        latitude: params.latitude,
-        longitude: params.longitude,
-        radius: params.radius,
-        low_price: params.price_range[0],
-        high_price: params.price_range[1]
-    }
+  const data = {
+    latitude: params.latitude,
+    longitude: params.longitude,
+    radius: params.radius,
+    low_price: params.price_range[0],
+    high_price: params.price_range[1]
+  }
 
-    axios.post('/api/destinations', data).then(res => {
-        dispatch({
-            type: constants.GET_DESTINATIONS,
-            payload: res.data
-        })
+  axios.post('/api/destinations', data).then(res => {
+    dispatch({
+      type: constants.GET_DESTINATIONS,
+      payload: res.data
     })
+  })
 }
 
 export const fetchUser = () => async dispatch => {
@@ -30,28 +30,28 @@ export const fetchUser = () => async dispatch => {
 export const handleToken = (token) => async dispatch => {
   const res = await axios.post('/api/stripe', token);
   dispatch({ type: FETCH_USER, payload: res.data });
-export const bookTrip = params => dispatch => {
+  export const bookTrip = params => dispatch => {
     console.log('params', params)
     const data = {
-        user_id: params.user_id,
-        destinations: params.destinations,
-        total_price: params.total_price
+      user_id: params.user_id,
+      destinations: params.destinations,
+      total_price: params.total_price
     };
 
     axios.post('/api/trip/book', data).then(res => {
-        alert('Successfully booked trip! Have fun!')
-    })
-}
+      alert('Successfully booked trip! Have fun!')
+    });
+  }
 
-export const handleToken = (token) => async dispatch =>{
+  export const handleToken = (token) => async dispatch => {
     const res = await axios.post('/api/stripe', token);
-  dispatch({ type: FETCH_USER, payload: res.data });
-};
+    dispatch({ type: FETCH_USER, payload: res.data });
+  };
 
-export const submitSurvey = (values, history) => async dispatch => {
-  const res = await axios.post('/api/surveys', values);
+  export const submitSurvey = (values, history) => async dispatch => {
+    const res = await axios.post('/api/surveys', values);
 
-  history.push('/surveys');
-  dispatch({ type: FETCH_USER, payload: res.data });
+    history.push('/surveys');
+    dispatch({ type: FETCH_USER, payload: res.data });
 
-};
+  }};
